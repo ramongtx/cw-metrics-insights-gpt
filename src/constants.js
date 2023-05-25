60d972b13b3097925b9cd768485ce15896e35925
Hongyi Duan <dhongyi@amazon.com>
2023-05-25 11:19:45 +0100

    provide 1 example

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-export const PROMPT_PREFIX = "Translate this to CloudWatch Metrics Insights query language: "
+export const PROMPT_PREFIX = ```
+This is example Metrics insights query to find the top 10 MAX EndToEndLatency metrics of namespace Cell-Alpha/Guppy/E2ECanary group by PartitionId order by MAX
+"SELECT MAX(EndToEndLatency) FROM "Cell-Alpha/Guppy/E2ECanary" GROUP BY PartitionId ORDER BY MAX() DESC LIMIT 10"
+Please follow the syntax and translate this to CloudWatch Metrics Insights query language: 
+```
 export const PROMPT_SUBFIX = " Respond with only the query."
